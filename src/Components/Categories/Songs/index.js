25b2968db358e078cafdf804004d531679398172
rabab-1993 <Rabab.Abdulaziz.elq.js@tuwaiq.edu.sa>
2021-11-12 19:53:58 +0300

    songs updated

--- src/Components/Categories/Songs/index.js
+++ src/Components/Categories/Songs/index.js
@@ -1,11 +1,43 @@
-import React from 'react'
-import './style.css';
-const index = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./style.css";
+const Songs = () => {
+  const navigate = useNavigate();
+  const [songs, setSongs] = useState([]);
 
-export default index
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const item = await axios.get("http://localhost:5500/songs");
+
+    setSongs(item.data);
+    console.log(songs);
+  };
+
+  // artist info
+
+  const info = (name) => {
+    // navigate(`name?artistName=${name}`)
+    console.log(name);
+  };
+
+  const r = [];
+  return (
+    <div>
+      {songs.map((song) => {
+        if (!r.includes(song.artistName)) {
+          r.push(song.artistName);
+          return (
+            <button onClick={info(song.artistName)}>{song.artistName}</button>
+          );
+        }
+      })}
+    </div>
+  );
+};
+
+export default Songs;
